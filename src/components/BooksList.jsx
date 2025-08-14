6d20eb367e4ba7d05e65009835037713128ac795
Carlos Leal <carlosleal2342@gmail.com>
2025-08-14 13:24:15 -0400

    Update in Booklist component

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,11 +1,18 @@
 import { library } from "../data/books.json";
+import { useReadingList } from "../hooks/useReadingList";
 import { Book } from "./Book";
 
 export const BooksList = () => {
+  const { readingList } = useReadingList();
+
+  const libraryFiltered = library.filter(
+    (book) => !readingList.some((item) => item.ISBN === book.book.ISBN)
+  );
+
   return (
     <div className="container">
       <div className="row">
-        {library.map(({ book }) => (
+        {libraryFiltered.map(({ book }) => (
           <Book key={book.ISBN} {...book} book={book} />
         ))}
       </div>
